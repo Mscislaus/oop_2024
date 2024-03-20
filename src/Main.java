f738b5471a855c6d3ed2f4530e004a9e8982efea
John Doe <johndoe@example.com>
2024-03-20 16:55:12 +0100

    +

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class Main extends Segment {
         Point end = new Point(2, 3);
         Segment segment = new Segment(begin, end);
         System.out.println(segment.toSvg());
+
         Point[] pointsArray = {new Point(50, 50), new Point(50, 20), new Point(100, 20),new Point(100, 50)};
         Polygon polygon = new Polygon(pointsArray);
         System.out.println(polygon.toSvg());
+        Polygon polygon2 = new Polygon(polygon);
+        System.out.println(polygon2.toSvg());
     }
 }
--- src/Polygon.java
+++ src/Polygon.java
@@ -10,10 +10,17 @@ public class Polygon {
     public String toSvg() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("<polygon points=\"");
-        for(Point point : points) {
+        for (Point point : points) {
             stringBuilder.append(point.x).append(",").append(point.y).append(" ");
         }
         stringBuilder.append("\" style=\"fill:none;stroke:black;stroke-width:1\"/>\n");
         return stringBuilder.toString();
+    }
+
+    public Polygon(Polygon src) {
+        this.points = new Point[src.points.length];
+        for(int i = 0; i < points.length; ++i) {
+            points[i] = new Point(src.points[i].x, src.points[i].y);
+        }
     }
 }
