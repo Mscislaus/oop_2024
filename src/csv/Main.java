8e78d5e31d16758d17ac2637e80eae2054ef3183
Mscislaus <76906961+Mscislaus@users.noreply.github.com>
2024-04-12 15:19:19 +0200

    +

---
+++ src/csv/Main.java
@@ -0,0 +1,12 @@
+package csv;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = Person.fromCsv("family.csv");
+
+        for(Person person: people)
+            System.out.println(person);
+    }
+}
--- src/csv/Person.java
+++ src/csv/Person.java
@@ -1,7 +1,12 @@
 package csv;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
@@ -14,13 +19,45 @@ public class Person {
         this.name = name;
         this.birthDate = birthDate;
         this.deathDate = deathDate;
-    };
+        this.parents = new ArrayList<>();
+    }
+    public static Person fromCsvLine(String csvLine){
+        String[] personList=csvLine.split(",",-1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthDate = LocalDate.parse(personList[1],formatter);
+        LocalDate deathDate =personList[2].equals("") ? null : LocalDate.parse(personList[2],formatter);
+        return new Person(personList[0],birthDate,deathDate);
+    }
+    public static List<Person> fromCsv(String path){
+        FileReader file = null;
+        BufferedReader buffered = null;
+        String linia = "";
+        List<Person> personList= new ArrayList<>();
+        try {
+            file = new FileReader(path);
+            buffered = new BufferedReader(file);
+            buffered.readLine();
+            while((linia = buffered.readLine()) != null){
+                Person person = fromCsvLine(linia);
+                personList.add(person);
+            }
+            if (buffered != null) buffered.close();
 
-    public static Person fromCsvLine(String csvLine) {
-        String[] stringArray = csvLine.split(",", -1);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.mm.yyyy");
-        LocalDate birthDate = LocalDate.parse(stringArray[1], formatter);
-        LocalDate deathDate = stringArray[2].equals("") ? null : LocalDate.parse(stringArray[2], formatter);
-        return new Person(stringArray[0], birthDate, deathDate);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return personList;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", deathDate=" + deathDate +
+                ", parents=" + parents +
+                '}';
     }
 }
+
